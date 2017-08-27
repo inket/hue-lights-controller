c224b893057ac36848753572ca850b5efd1191c7
Caleb Lemoine <circa10a@users.noreply.github.com>
2017-08-27 14:58:38 -0500

    Create hue.sh

---
+++ hue.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+###Docs for getting token https://developers.meethue.com/documentation/getting-started
+###Possible Light States https://github.com/sqmk/huejay
+
+#Detects Bridge IP automatically
+bridgeIP=$(curl -s https://www.meethue.com/api/nupnp | cut -d: -f3 | sed 's/"//g' | sed 's/}//g' | sed 's/]//g')
+
+#REQUIRED - Get your own token, see link at the top
+#Example Token
+userToken="8BGXPDAKtXVoBdlDlrauoPJP-6JFENcEq4m6F05t"
+
+getLightsInfo(){
+#Get all light attributes
+curl http://$bridgeIP/api/$userToken/lights/
+}
+
+#My Light ID's, yours will differ. Call getLightsInfo()
+lights=(3 4 5)
+
+getLightStatus(){
+  light=$2
+  curl http://$bridgeIP/api/$userToken/lights/$2
+}
+
+getAllLightStatus(){
+#Get individual attributes
+for i in ${lights[@]}
+do
+  curl http://$bridgeIP/api/$userToken/lights/$i
+done
+}
+
+lightOn(){
+#Turn on/change brightness
+curl -H 'Content-Type: application/json' -X PUT -d '{"on":true, "bri":200}' http://$bridgeIP/api/$userToken/lights/$3/state
+}
+
+lightsOn(){
+for i in ${lights[@]}
+do
+  curl -H 'Content-Type: application/json' -X PUT -d '{"on":true, "bri":200}' http://$bridgeIP/api/$userToken/lights/$i/state
+done
+}
+
+lightsOff(){
+for i in ${lights[@]}
+do
+  curl -H 'Content-Type: application/json' -X PUT -d '{"on":false}' http://$bridgeIP/api/$userToken/lights/$i/state
+done
+}
+
+#Call Function
+$1
