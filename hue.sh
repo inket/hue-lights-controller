8bb1af891a59d0c0ed5adeb1ef2524bf387d35d7
Caleb Lemoine <21261388+circa10a@users.noreply.github.com>
2020-05-15 23:07:45 -0500

    fix indentation
    fix indentation, cant be believe i wrote gross code like this

--- hue.sh
+++ hue.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-###Docs for getting token https://developers.meethue.com/documentation/getting-started
-###Possible Light States https://github.com/sqmk/huejay#lights
+# Docs for getting token https://developers.meethue.com/documentation/getting-started
+# Possible Light States https://github.com/sqmk/huejay#lights
 
-#Detects Bridge IP automatically
+# Detects Bridge IP automatically
 bridgeIP=$(curl -s https://www.meethue.com/api/nupnp | cut -d: -f3 | tr -d '"}]')
 
-#REQUIRED - Get your own token, see link at the top
-#Example Token
+# REQUIRED - Get your own token, see link at the top
+# Example Token
 userToken="8BGXPDAKtXVoBdlDlrauoPJP-6JFENcEq4m6F05t"
 
 getLightsInfo(){
-#Get all light attributes
-curl http://$bridgeIP/api/$userToken/lights/
+  # Get all light attributes
+  curl http://$bridgeIP/api/$userToken/lights/
 }
 
-#My Light ID's, yours will differ. Call getLightsInfo()
+# My Light ID's, yours will differ. Call getLightsInfo()
 lights=(3 4 5)
 
 getLightStatus(){
@@ -23,36 +23,36 @@ getLightStatus(){
 }
 
 getAllLightStatus(){
-#Get individual attributes
-for i in ${lights[@]}
-do
-  curl http://$bridgeIP/api/$userToken/lights/$i
-done
+  # Get individual attributes
+  for i in ${lights[@]}
+  do
+    curl http://$bridgeIP/api/$userToken/lights/$i
+  done
 }
 
 lightOn(){
-#Turn on/change brightness single light
-curl -H 'Content-Type: application/json' -X PUT -d '{"on":true, "bri":200}' http://$bridgeIP/api/$userToken/lights/$2/state
+  # Turn on/change brightness single light
+  curl -H 'Content-Type: application/json' -X PUT -d '{"on":true, "bri":200}' http://$bridgeIP/api/$userToken/lights/$2/state
 }
 
 lightOff(){
-#Turn off single light
-curl -H 'Content-Type: application/json' -X PUT -d '{"on":false}' http://$bridgeIP/api/$userToken/lights/$2/state
+  # Turn off single light
+  curl -H 'Content-Type: application/json' -X PUT -d '{"on":false}' http://$bridgeIP/api/$userToken/lights/$2/state
 }
 
 lightsOn(){
-for i in ${lights[@]}
-do
-  curl -H 'Content-Type: application/json' -X PUT -d '{"on":true, "bri":200}' http://$bridgeIP/api/$userToken/lights/$i/state
-done
+  for i in ${lights[@]}
+  do
+    curl -H 'Content-Type: application/json' -X PUT -d '{"on":true, "bri":200}' http://$bridgeIP/api/$userToken/lights/$i/state
+  done
 }
 
 lightsOff(){
-for i in ${lights[@]}
-do
-  curl -H 'Content-Type: application/json' -X PUT -d '{"on":false}' http://$bridgeIP/api/$userToken/lights/$i/state
-done
+  for i in ${lights[@]}
+  do
+    curl -H 'Content-Type: application/json' -X PUT -d '{"on":false}' http://$bridgeIP/api/$userToken/lights/$i/state
+  done
 }
 
-#Call Function
+# Call Function
 $1
